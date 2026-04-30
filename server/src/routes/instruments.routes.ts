4a45fe3fbbde52f2c53c81afe93f3487b7037cbd
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-04-30 23:13:52 +0200

    feat: add get single instrument by symbol endpoint

--- server/src/routes/instruments.routes.ts
+++ server/src/routes/instruments.routes.ts
@@ -7,4 +7,20 @@ instumentsRouter.get("/", (req, res) => {
   res.json(instruments);
 });
 
+instumentsRouter.get("/:symbol", (req, res) => {
+  const { symbol } = req.params;
+
+  let instrument = instruments.find(
+    (instrument) => instrument.symbol === symbol,
+  );
+
+  if (!instrument) {
+    res.status(404).json({ message: "Instrument not found" });
+  }
+
+  res.status(200).json({
+    instrument,
+  });
+});
+
 export default instumentsRouter;
