a06dac8c748001e07b6dc6627e6a8e816ea42995
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-06-13 16:37:24 +0200

    deposite money or stock to the account - GET /api/v1/balance/deposite

--- backend/http-server/src/routes/balance.routes.ts
+++ backend/http-server/src/routes/balance.routes.ts
@@ -2,8 +2,12 @@ import { Router, type Response } from "express";
 import type { AuthRequest } from "../types-interfaces/types";
 import { getUserBalance, serializeBalance } from "../helper/helpers";
 import { balances } from "../data/in-memory-database";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { DepositSchema, type DepositInput } from "../zod-schemas/zod";
 
 const router = Router();
+
+router.use(authMiddleware);
 
 //router 4: GET /api/v1/balance
 
@@ -11,6 +15,33 @@ router.get("/", (req: AuthRequest, res: Response): void => {
   const ub = getUserBalance(req.user!.userId);
 
   res.json({ balances: serializeBalance(ub) });
+});
+
+//router 5: POST /api/v1/balance/deposit
+
+router.post("/deposit", (req: AuthRequest, res: Response): void => {
+  const parsed = DepositSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    res.status(400).json({
+      message: "Invalid input",
+      errors: parsed.error.issues,
+    });
+
+    return;
+  }
+
+  const { asset, amount }: DepositInput = parsed.data;
+
+  const ub = getUserBalance(req.user!.userId);
+
+  const current = ub.assets.get(asset)!;
+
+  current.available += amount;
+
+  ub.assets.set(asset, current);
+
+  res.json({ message: "Deposit successful", balance: current });
 });
 
 export default router;
--- backend/http-server/src/zod-schemas/zod.ts
+++ backend/http-server/src/zod-schemas/zod.ts
@@ -1,4 +1,4 @@
-import { email, z } from "zod";
+import z from "zod";
 
 export const SignupSchema = z.object({
   email: z.string().email("Invalid email id"),
@@ -9,5 +9,11 @@ export const SigninSchema = z.object({
   email: z.string().email("Invalid email id"),
   password: z.string().min(8, "Password must me atleast 6 charactes"),
 });
+
+export const DepositSchema = z.object({
+  asset: z.enum(["INR", "TATA", "PAYTM", "ZOMATO"]),
+  amount: z.number().positive(),
+});
 export type SignupInput = z.infer<typeof SignupSchema>;
 export type SigninInput = z.infer<typeof SigninSchema>;
+export type DepositInput = z.infer<typeof DepositSchema>;
