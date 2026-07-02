f2fcb7ee3a77ae38d786afb5ae73c7ba674a3148
Lalit kumar <lk4244373@gmail.com>
2026-07-02 23:35:10 +0200

    prisma client -- singelTon pattern

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient | null = null;
+
+export const getPrisma = (): PrismaClient => {
+  if (!prisma) {
+    prisma = new PrismaClient({
+      log: ["error", "warn"],
+    });
+  }
+
+  return prisma;
+};
+export type {
+  User,
+  Order,
+  Balance,
+  OrderKind,
+  OrderType,
+  OrderStatus,
+  Market,
+  Asset,
+} from "@prisma/client";
